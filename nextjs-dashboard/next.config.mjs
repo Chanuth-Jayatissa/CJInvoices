a1e457bc49d64649e57b11362e6964e129e5e239
Chanuth Jayatissa <chanuthjayatissa@gmail.com>
2024-07-26 04:20:37 -0400

    fixing vercel error final

--- nextjs-dashboard/next.config.mjs
+++ nextjs-dashboard/next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    /*  // Partial Prerendering (PPR) does not work with vercel yet
+    experimental: {
+        ppr: 'incremental',
+    }, 
+    */
+};
 
 export default nextConfig;

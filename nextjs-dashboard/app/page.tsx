a92c6e46da4c73329c6b5a0ec5c3878736189272
Chanuth Jayatissa <chanuthjayatissa@gmail.com>
2024-08-08 14:33:37 -0400

    small change

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -12,20 +12,15 @@ export default function Page() {
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className={styles.shape} />
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the {' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course 
-            </a>
-            , brought to you by Vercel.
+        <div className="flex flex-col justify-start gap-6 rounded-lg bg-gray-50 px-6 py-20 md:w-2/5 md:px-20">
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal mt-[150px]`}>
+            <strong>Welcome to CJ Invoices.</strong> This is my first project that manages customer invoices.
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-center mt-8 rounded-lg bg-blue-500 px-8 py-4 text-xl font-medium text-white transition-colors hover:bg-blue-400 md:text-2xl"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Log in</span> <ArrowRightIcon className="w-8 md:w-12 stroke-[1.5px] md:stroke-[2.5px]" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">

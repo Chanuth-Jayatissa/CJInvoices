110f65e3d4c774b0ad6d41e441abb71fe489037c
Chanuth Jayatissa <chanuthjayatissa@gmail.com>
2024-08-11 04:16:01 -0400

    fixed the logo text

--- nextjs-dashboard/app/ui/cjinvoices-logo.tsx
+++ nextjs-dashboard/app/ui/cjinvoices-logo.tsx
@@ -7,7 +7,7 @@ export default function CJInovicesLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px] ml-4">CJ Invoices</p>
+      <p className="text-3xl md:text-5xl ml-4">CJ Invoices</p>
     </div>
   );
 }
